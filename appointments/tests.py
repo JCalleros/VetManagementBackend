75e8bc2b81729fae554ffb3f7602d741364b6f97
JCalleros <jcalleros1009@gmail.com>
2024-03-11 16:59:51 -0700

    created appointments app and first test

---
+++ appointments/tests.py
@@ -0,0 +1,8 @@
+
+
+from django.test import TestCase
+from .models import Appointment
+# Create your tests here.
+class AppointmentTestCase(TestCase):
+    def setUp(self):
+        return
--- vet_management/settings.py
+++ vet_management/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'users',
     'pets',
+    'appointments',
 ]
 
 REST_FRAMEWORK = {
