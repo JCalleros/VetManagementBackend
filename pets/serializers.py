840dd54e81beaadeb8ff58d16e901eaff40fcfe8
JCalleros <jcalleros1009@gmail.com>
2024-03-25 05:36:25 -0700

    add pets and owners improvements

--- pets/serializers.py
+++ pets/serializers.py
@@ -4,32 +4,12 @@ from .models import Pet, Owner
 class OwnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Owner
-        fields = ['id', 'name', 'phone_number', 'contact', 'address']
+        fields = ('id', 'full_name', 'phone_number', 'contact', 'address')
 
 class PetSerializer(serializers.ModelSerializer):
-    owner = OwnerSerializer(allow_null=True, required=False)
+    owners = serializers.PrimaryKeyRelatedField(many=True, queryset=Owner.objects.all(), required=False)
 
     class Meta:
         model = Pet
-        fields = ['id', 'name', 'species', 'sex', 'breed', 'age_years', 'age_months', 'age_weeks', 'color', 'owner']
+        fields = ['id', 'name', 'species', 'sex', 'breed', 'age_years', 'age_months', 'age_weeks', 'color', 'owners']
 
-    def create(self, validated_data):
-        owner_data = validated_data.pop('owner', None)
-        owner = None
-        if owner_data and all(value is not None for value in owner_data.values()):
-            owner = Owner.objects.create(**owner_data) if owner_data else None
-        pet = Pet.objects.create(owner=owner, **validated_data)
-        return pet
-    
-    def update(self, instance, validated_data):
-        owner_data = validated_data.pop('owner', None)
-        if owner_data is not None:
-            owner = instance.owner
-            if owner is not None:
-                for attr, value in owner_data.items():
-                    setattr(owner, attr, value)
-                owner.save()
-            else:
-                owner = Owner.objects.create(**owner_data)
-                instance.owner = owner
-        return super().update(instance, validated_data)
--- pets/views.py
+++ pets/views.py
@@ -3,21 +3,53 @@ from .models import Pet, Owner
 from .serializers import PetSerializer, OwnerSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 from vet_management.helpers import CookieJWTAuthentication
+
 
 class PetViewSet(viewsets.ModelViewSet):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name', 'species', 'sex', 'owner__name']
+    filterset_fields = ['name', 'species', 'sex', 'owners__full_name', 'owners__phone_number']
+    ordering_fields = ['name', 'species']
     authentication_classes = [CookieJWTAuthentication]
     permission_classes = [IsAuthenticated]
 
- 
+    def create(self, request, *args, **kwargs):
+        name = request.data.get('name')
+        species = request.data.get('species')
+        sex = request.data.get('sex')
+        owners = request.data.get('owners')
+        if owners is not None:
+            if Pet.objects.filter(name=name, species=species, sex=sex, owners__in=owners).exists():
+                return Response({"detail": "A pet with these details already exists."}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            if Pet.objects.filter(name=name, species=species, sex=sex, owners__isnull=True).exists():
+                return Response({"detail": "A pet with these details already exists."}, status=status.HTTP_400_BAD_REQUEST)
+                
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        name = request.data.get('name')
+        species = request.data.get('species')
+        sex = request.data.get('sex')
+        owners = request.data.get('owners')
+        if owners is not None:
+            if Pet.objects.filter(name=name, species=species, sex=sex, owners__in=owners).exclude(id=kwargs['pk']).exists():
+                return Response({"detail": "A pet with these details already exists."}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            if Pet.objects.filter(name=name, species=species, sex=sex, owners__isnull=True).exclude(id=kwargs['pk']).exists():
+                return Response({"detail": "A pet with these details already exists."}, status=status.HTTP_400_BAD_REQUEST)
+        return super().update(request, *args, **kwargs)
+
+    
+    
 class OwnerViewSet(viewsets.ModelViewSet):
     queryset = Owner.objects.all()
     serializer_class = OwnerSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name']
+    filterset_fields = ['full_name', 'phone_number']
     authentication_classes = [CookieJWTAuthentication]
     permission_classes = [IsAuthenticated]
