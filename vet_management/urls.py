6109507238f90b5117a677d4c65fc9c67907ec45
JCalleros <jcalleros1009@gmail.com>
2024-06-12 15:30:55 -0700

    Add api rest documentation

--- vet_management/urls.py
+++ vet_management/urls.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="vetSystem API",
+      default_version='v1',
+      description="vetSystem API Documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +25,8 @@ urlpatterns = [
     path('api/v1/appointments/', include('appointments.urls'))
 ]
 
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+   urlpatterns += [
+      path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='scheme-swagger'),
+      path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   ]
